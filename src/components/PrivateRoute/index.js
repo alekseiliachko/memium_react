8076d022ac25e57f4af2fb491ebeb8b874d477ad
alex <alexander.korichev@gmail.com>
2020-12-14 04:16:08 +0300

    check auth state

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,3 +1,27 @@
 import * as React from "react";
+import { Redirect, Route } from "react-router";
+import { AUTH_STATE } from "../../redux/auth/reducer";
 
-export const PrivateRoute = ({ isAuth }) => {};
+export const PrivateRoute = ({ authState, children, ...rest }) => {
+  if (authState === AUTH_STATE.ATTEMPT) {
+    return null;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        authState === AUTH_STATE.SUCCESS ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -1,23 +1,33 @@
-import { LOGIN_SUCCESS, LOG_OUT } from "./actions";
+import { LOGIN_ATTEMPT, LOGIN_SUCCESS, LOG_OUT } from "./actions";
+
+export const AUTH_STATE = {
+  LOGGED_OUT: "LOGGED_OUT",
+  SUCCESS: "SUCCESS",
+  ATTEMPT: "ATTEMPT",
+};
 
 const initialState = {
   username: "",
-  isAuthPresent: false,
+  authState: AUTH_STATE.ATTEMPT,
 };
 
 export const authReducer = (store = initialState, action) => {
   switch (action.type) {
+    case LOGIN_ATTEMPT:
+      return {
+        ...store,
+        authState: AUTH_STATE.ATTEMPT,
+      };
     case LOGIN_SUCCESS:
       return {
         ...store,
         username: action.payload,
-        isAuthPresent: true,
+        authState: AUTH_STATE.SUCCESS,
       };
     case LOG_OUT:
       return {
         ...store,
-        username: "",
-        isAuthPresent: false,
+        username: AUTH_STATE.LOGGED_OUT,
       };
     default:
       return store;
