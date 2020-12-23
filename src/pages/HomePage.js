c72b721bec871d11f40dfdfac3e054d94541a3c5
korichevalex <Alexander Korichev>
2020-12-24 00:47:23 +0300

    мелкие правки

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,6 @@ import { Container } from "@material-ui/core";
 import React from "react";
 import { AppHeaderContainer } from "../containers/AppHeaderContainer";
 import { FeedContainer } from "../containers/FeedContainer";
-import { CategorySelect } from "../components/CategorySelect";
 
 export const HomePage = () => {
   return (
